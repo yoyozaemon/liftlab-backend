6da79384230be4914959bfc10cf0b6e43e0e849f
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2026-01-26 08:29:13 +0530

    Update index.go

--- src/config/index.go
+++ src/config/index.go
@@ -13,10 +13,18 @@ var (
 )
 
 func LoadConfig() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Error loading .env file")
+	if _, err := os.Stat(".env"); err == nil {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("Error loading .env file")
+		}
+	} else {
+		log.Println(".env file not found, using environment variables")
 	}
+
 	PORT = os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "3000"
+	}
 	MONGODB_URI = os.Getenv("MONGODB_URI")
 }
